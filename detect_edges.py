d192570469e1452deda925a1a26a49cd96b21a20
Danila Kurganov <dsk19@ic.ac.uk>
2022-10-17 22:01:50 +0100

    removed redundant sentence

--- detect_edges.py
+++ detect_edges.py
@@ -95,7 +95,6 @@ def reorganise_frame(full_frame, height_unit, width_unit, height, width):
 
 def label_frame(frame, text, position):
     font                   = cv2.FONT_HERSHEY_SIMPLEX
-    bottomLeftCornerOfText = (10,500)
     fontScale              = 1
     fontColor              = (255,255,255)
     thickness              = 1
